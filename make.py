1790b574ee77f9bb7f7ad3faa1278d1f2f3a79f3
AirSheep <80259024+6IXK1LL@users.noreply.github.com>
2022-03-13 18:26:38 +0300

    Update make.py

--- make.py
+++ make.py
@@ -1,13 +1,10 @@
 from telegraph import Telegraph
 from parser import Parser
 
+from typing import List
 
-def make(URL):
-    parser = Parser(URL).run()
 
-    title = parser[0]
-    content = parser[1]
-
+def create(title, content) -> str:
     telegraph = Telegraph()
     telegraph.create_account(short_name='1337')
 
@@ -19,3 +16,16 @@ def make(URL):
     url = 'http://telegra.ph/{}'.format(response['path'])
 
     return url
+
+def make(URL) -> str:
+    parser: List = Parser(URL).run()
+
+    title: str = parser[0]
+    content: str = parser[1]
+
+    article = create(title, content)
+    return article
+
+
+if __name__=='__main__':
+    print( make('https://habr.com/ru/company/jugru/blog/649789/') )
